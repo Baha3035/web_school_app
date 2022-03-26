6c5ae045ca94a630fcbc11abe1c18addd5336511
Baha <batmaaanpls@gmail.com>
2022-03-26 21:16:47 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask,render_template,request,redirect,url_for,flash
+import sqlite3 as sql
+app=Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    con=sql.connect("db_web.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from students")
+    data=cur.fetchall()
+    return render_template("index.html",datas=data)
+
+@app.route("/add_user",methods=['POST','GET'])
+def add_user():
+    if request.method=='POST':
+        name=request.form['name']
+        surname=request.form['surname']
+        date_of_birth=request.form['date_of_birth']
+        class_s=request.form['class']
+        address=request.form['address']
+        gender=request.form['gender']
+        con=sql.connect("db_web.db")
+        cur=con.cursor()
+        cur.execute("insert into students(NAME,SURNAME,DATE_OF_BIRTH,CLASS,ADDRESS,GENDER) values (?,?,?,?,?,?)",(name,surname,date_of_birth,class_s,address,gender))
+        con.commit()
+        flash('Student Added','success')
+        return redirect(url_for("index"))
+    return render_template("add_user.html")
+
+@app.route("/edit_user/<string:uid>",methods=['POST','GET'])
+def edit_user(uid):
+    if request.method=='POST':
+        name=request.form['name']
+        surname=request.form['surname']
+        date_of_birth=request.form['date_of_birth']
+        class_s=request.form['class']
+        address=request.form['address']
+        gender=request.form['gender']
+        con=sql.connect("db_web.db")
+        cur=con.cursor()
+        cur.execute("update students set NAME=?,SURNAME=?,DATE_OF_BIRTH=?,CLASS=?,ADDRESS=?,GENDER=? where UID=?",(name,surname,date_of_birth,class_s,address,gender,uid))
+        con.commit()
+        flash('Student Updated','success')
+        return redirect(url_for("index"))
+    con=sql.connect("db_web.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from students where UID=?",(uid,))
+    data=cur.fetchone()
+    return render_template("edit_user.html",datas=data)
+    
+@app.route("/delete_user/<string:uid>",methods=['GET'])
+def delete_user(uid):
+    con=sql.connect("db_web.db")
+    cur=con.cursor()
+    cur.execute("delete from students where UID=?",(uid,))
+    con.commit()
+    flash('User Deleted','warning')
+    return redirect(url_for("index"))
+    
+if __name__=='__main__':
+    app.secret_key='admin123'
+    app.run(debug=True)
---
+++ create_db.py
@@ -0,0 +1,36 @@
+import sqlite3 as sql
+
+#connect to SQLite
+con = sql.connect('db_web.db')
+
+#Create a Connection
+cur = con.cursor()
+
+#Drop users table if already exsist.
+cur.execute("DROP TABLE IF EXISTS students")
+
+#Create users table  in db_web database
+sql ='''CREATE TABLE "students" (
+	"UID"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"NAME"	TEXT,
+	"SURNAME"	TEXT,
+    "DATE_OF_BIRTH" TEXT,
+    "CLASS" TEXT,
+    "ADDRESS" TEXT,
+    "GENDER" TEXT
+)'''
+cur.execute(sql)
+
+sql ='''CREATE TABLE "admins" (
+	"UID"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"NAME"	TEXT,
+    "LOGIN" TEXT,
+    "PASSWORD" TEXT
+)'''
+cur.execute(sql)
+
+#commit changes
+con.commit()
+
+#close the connection
+con.close()
